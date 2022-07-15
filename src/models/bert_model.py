dc8fa6bb96691ce96e7a285dbb3befa1a0c1696b
Nathan B <nate.physics@berkeley.edu>
2022-07-15 15:51:24 +0200

    removed criterion, updated num_labels and mettrics

--- src/models/bert_model.py
+++ src/models/bert_model.py
@@ -11,9 +11,8 @@ class BertModel(pl.LightningModule):
 
     def __init__(
         self, 
-        num_labels: int = None, 
+        num_labels: int, 
         log_training: Optional[bool] = False,
-        criterion: Optional[DictConfig] = {},
         optimizer: Optional[DictConfig] = {},
         scheduler: Optional[DictConfig] = {},
         metrics: Optional[DictConfig] = {},
@@ -21,14 +20,11 @@ class BertModel(pl.LightningModule):
 
         super(BertModel, self).__init__()
         self.save_hyperparameters(
-            "criterion", "optimizer", "scheduler", "metrics"
+            "optimizer", "scheduler", "metrics"
         )
 
         # load the model
         self.bert = BertForTokenClassification.from_pretrained('bert-base-cased', num_labels=num_labels)
-        
-        # initialize the criterion
-        self.criterion = instantiate(criterion)
 
         # metrics
         metrics = instantiate_list(metrics)
@@ -49,24 +45,32 @@ class BertModel(pl.LightningModule):
 
     def forward(self, input_id, mask, label):
         """Forward pass of the model."""
-        output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
+        loss, output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
 
-        return output
+        return output, loss
 
     def step(self, batch: any, batch_idx: int):
         # get the inputs
         data, true_label = batch
         # forward pass
-        pred_label = self.forward(data["input_ids"][0], data["attention_mask"][0], data["labels"][0])
+        output, loss = self.forward(data["input_ids"][0], data["attention_mask"][0], data["labels"][0])
         # compute the loss
-        loss = self.criterion(pred_label, true_label)
+        output_clean = output[0][output != -100]
+        label_clean = true_label[true_label != -100]
+
+        predictions = output_clean.argmax(dim=1)
+
+        acc = (predictions == label_clean).float().mean()
+        self.log("train/acc", acc)
+
+
         if torch.isnan(loss):
             print('Issue')
-        return loss, pred_label, true_label
+        return loss, predictions, label_clean
 
     def training_step(self, batch, batch_idx):
         loss, pred_label, true_label = self.step(batch, batch_idx)
-        self.log("train/loss", loss)
+        self.log("train/loss", loss, prog_bar=True, logger=True)
 
         # log training metrics (if enabled)
         if self.log_training:
@@ -79,20 +83,16 @@ class BertModel(pl.LightningModule):
                 if batch_idx != 0:
                     self.log(f"train/{layer}.max_grad", torch.max(param.grad))
 
-        return loss
+        return {"loss": loss, "predictions": pred_label, "labels": true_label}
 
     def validation_step(self, batch: any, batch_idx: int):
         loss, pred_label, true_label = self.step(batch, batch_idx)
-        self.log("val/loss", loss)
-        self.val_metrics(pred_label, true_label)
-        self.log_dict(self.val_metrics)
+        self.log("val/loss", loss, prog_bar=True, logger=True)
         return loss
 
     def test_step(self, batch: any, batch_idx: int):
         loss, pred_label, true_label = self.step(batch, batch_idx)
-        self.log("test/loss", loss)
-        self.test_metrics(pred_label, true_label)
-        self.log_dict(self.test_metrics)
+        self.log("test/loss", loss, prog_bar=True, logger=True)
         return loss
         
     def configure_optimizers(self):
